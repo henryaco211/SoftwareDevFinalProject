e58be72bb39eb3310ff6d58ec89a035b3bfaa56a
henryaco211 <henryaco211@gmail.com>
2024-04-28 19:06:54 -0400

    Version 1.0

--- finalJavaScriptSignUp.js
+++ finalJavaScriptSignUp.js
@@ -1,9 +1,7 @@
 /////////Sign Up Script//////////
 
-// Get the modal
 var modal = document.getElementById('id01');
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) 
 	{
 	if (event.target == modal) 
